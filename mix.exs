77ee570b35b89985aac68ebbd2e3fb55b808b382
Tom Clarke <tom@u2i.com>
2025-04-28 10:43:58 -0400

    Added livex

--- mix.exs
+++ mix.exs
@@ -40,6 +40,7 @@ defmodule LivexDemo.MixProject do
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.0.9"},
+      {:livex, github: "u2i/livex"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.9", runtime: Mix.env() == :dev},
